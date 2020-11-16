02309a7fc5808f16c79c5a7b467aa3ecb8e48f91
Vladimir Grunev <vgrunev@oneincsystems.com>
2020-11-16 19:48:38 +0700

    Add 2 tasks

---
+++ DynamicProgramming/ConstrainedSubsetSum.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int ConstrainedSubsetSum(int[] nums, int k)
+    {
+        var n = nums.Length;
+        var dp = new long?[n];
+
+        long Solve(int[] nums, int i, int k)
+        {
+            if (i == nums.Length) return 0;
+            if (dp[i].HasValue) return dp[i].Value;
+
+            long bestSolution = 0;
+            for (int j = i + 1; j < Math.Min(n, i + k + 1); j++)
+            {
+                bestSolution = Math.Max(Solve(nums, j, k), bestSolution);
+                if (nums[j] >= 0)
+                {
+                    break;
+                }
+            }
+
+            dp[i] = nums[i] + bestSolution;
+            return dp[i].Value;
+        }
+
+        var result = Solve(nums, 0, k);
+        for (int i = 1; i < n; i++)
+        {
+            result = Math.Max(Solve(nums, i, k), result);
+        }
+
+        return (int) result;
+    }
+}
---
+++ DynamicProgramming/PalindromePartition.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    private int[,] dp = new int[101, 101];
+
+    public int PalindromePartition(string s, int k)
+    {
+        for (int i = 0; i < 101; i++)
+        {
+            for (int j = 0; j < 101; j++)
+            {
+                dp[i, j] = int.MaxValue;
+            }
+        }
+
+        return FindSolution(s, k);
+    }
+
+    public int FindSolution(string s, int k)
+    {
+        if (k == 1) return MakePalindrome(s);
+        var n = s.Length;
+        if (dp[k, n] != int.MaxValue) return dp[k, n];
+        var result = int.MaxValue;
+
+        for (int i = 1; i <= n - k + 1; i++)
+        {
+            var c = FindSolution(s.Substring(0, i), 1);
+            var sp = FindSolution(s.Substring(i, n - i), k - 1);
+            result = Math.Min(result, c + sp);
+        }
+
+        dp[k, n] = result;
+
+        return result;
+    }
+
+
+    public int MakePalindrome(string s)
+    {
+        var c = 0;
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1]) c++;
+        }
+
+        return c;
+    }
+}
